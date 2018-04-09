b2d099426417a6eb690d3d59a9f4b63c97ede2c7
Stefan Kahl <kahst@hrz.tu-chemnitz.de>
2018-04-09 13:29:37 +0200

    Baseline config

--- config.py
+++ config.py
@@ -17,10 +17,10 @@ def getRandomState():
 # Path settings (train audio and xml files, specs, test audio files and json metadata)
 # Use 'sort_data.py' to organize the BirdCLEF dataset accordingly
 # Extract the BirdCLEF TrainingSet data into TRAINSET_PATH
-TRAINSET_PATH = 'dataset/TrainingSet/'
-DATASET_PATH = 'dataset/spec/'
-TESTSET_PATH = 'dataset/val/'
-METADATA_PATH = 'dataset/metadata/'
+TRAINSET_PATH = 'datasets/TrainingSet/'
+DATASET_PATH = 'datasets/spec/'
+TESTSET_PATH = 'datasets/val/'
+METADATA_PATH = 'datasets/metadata/'
 
 # Maximum number of classes to use
 MAX_CLASSES = 250
@@ -28,8 +28,8 @@ MAX_CLASSES = 250
 # If not sorted, using only a subset of classes (MAX_CLASSES) will select classes randomly
 SORT_CLASSES_ALPHABETICALLY = False  
 
-# Specify minimum and maximum amount of samples per class
-MIN_SAMPLES_PER_CLASS = -1                                        
+# Specify minimum and maximum amount of samples (specs) per class
+MIN_SAMPLES_PER_CLASS = -1  # -1 = no minimum                                      
 MAX_SAMPLES_PER_CLASS = 500 # None = no limit
 
 # Specify the signal-to-noise interval you want to pick samples from (filename contains value)
@@ -41,7 +41,7 @@ VAL_SPLIT = 0.05
 ######################  SPECTROGRAMS  ######################
 
 # Sample rate for recordings, other sampling rates will force re-sampling
-SAMPLE_RATE = 41000
+SAMPLE_RATE = 44100
 
 # Specify min and max frequency for low and high pass
 SPEC_FMIN = 500
@@ -49,13 +49,13 @@ SPEC_FMAX = 15000
 
 # Define length of chunks for spec generation, overlap of chunks and chunk min length
 SPEC_LENGTH = 1.0
-SPEC_OVERLAP = 0.0
+SPEC_OVERLAP = 0.25
 SPEC_MINLEN = 1.0
 
 # Threshold for distinction between noise and signal
-SPEC_SIGNAL_THRESHOLD = 0.01
+SPEC_SIGNAL_THRESHOLD = 0.001
 
-# Limit the amount of specs per class (None = no limit)
+# Limit the amount of specs per class when extracting spectrograms (None = no limit)
 MAX_SPECS_PER_CLASS = 1000
 
 #########################  IMAGE  #########################
@@ -73,27 +73,27 @@ IM_SIZE = (256, 128)
 # 'fill': Fills with random noise to keep aspect ratio
 RESIZE_MODE = 'squeeze'
 
-# Normalization mode
+# Normalization mode (values between -1 and 1)
 ZERO_CENTERED_NORMALIZATION = True
 
 # Image augmentation, uncomment to use; specify mode + value
-IM_AUGMENTATION = {#'roll_h':0.5,
-                   'roll_v':0.1,
-                   #'crop':[0.1, 0.0, 0.05, 0.0],
-                   'noise':0.05,
-                   #'brightness':0.15,
-                   #'dropout':0.25,
-                   #'blackout':0.10,
-                   #'blur':3,
-                   #'zoom':0.25,
-                   #'rotate':10,
-                   #'multiply':0.25,
-                   #'mean':True
+IM_AUGMENTATION = {#'roll_h':0.5,                   # Horizontal roll
+                   'roll_v':0.1,                    # Vertical roll
+                   #'crop':[0.1, 0.0, 0.05, 0.0],   # Random crop - top, left, bottom, right
+                   #'noise':0.05,                   # Gaussian noise
+                   #'brightness':0.15,              # Adjust brightness
+                   #'dropout':0.25,                 # Dropout single pixels
+                   #'blackout':0.10,                # Dropout entire regions
+                   #'blur':3,                       # Image blur
+                   #'zoom':0.25,                    # Random zoom (equally cropping each side)
+                   #'rotate':10,                    # Rotate by angle
+                   #'multiply':0.25,                # Multiply pixel values
+                   #'mean':True                     # Substract mean from image
                   }
 
 # Maximum number of random augmentations per image
 # Each try has 50% chance of success; we do not use duplicate augmentations
-AUGMENTATION_COUNT = 2
+AUGMENTATION_COUNT = 1
 
 # Probability for image augmentation
 AUGMENTATION_PROBABILITY = 0.5
@@ -131,7 +131,7 @@ RUN_NAME = 'BirdCLEF_TUC_CLO_EXAMPLE'
 MODEL_PATH = 'snapshots/'
 
 # Filename of .pkl-file to load pre-trained model from (default = None, has to be the 'model_params'-file)
-PRETRAINED_MODEL_NAME = None
+PRETRAINED_MODEL_NAME = None # e.g. 'BirdCLEF_TUC_CLO_EXAMPLE_model_params_epoch_50.pkl'
 
 # If the output size of the pre-trained model differs from the current model, set flag to False
 LOAD_OUTPUT_LAYER = True
@@ -139,7 +139,7 @@ LOAD_OUTPUT_LAYER = True
 #######################  TRAINING  ########################
 
 # Number of epochs to train
-EPOCHS = 50
+EPOCHS = 60
 
 # Start epoch, important if you use a pre-trained model to continue training
 EPOCH_START = 1
@@ -153,7 +153,7 @@ LEARNING_RATE = {'start':0.001, 'end':0.000001}
 # Options are 'step', 'linear', 'cosine', 'root', 'constant'
 # If you want to use steps, write 'step-3' for three steps
 # during training to go from start to end lr
-LR_SCHEDULE = 'root' 
+LR_SCHEDULE = 'cosine' 
 
 # Impact of L2 measure on loss
 L2_WEIGHT = 0
@@ -162,17 +162,17 @@ L2_WEIGHT = 0
 OPTIMIZER = 'adam'
 
 # Epochs for snapshot save, [-1] = after every epoch
-SNAPSHOT_EPOCHS = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+SNAPSHOT_EPOCHS = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
 
 # Epochs to wait before early stopping
 EARLY_STOPPING_WAIT = 10
 
 ########################  TESTING  ########################
 
-# .pkl file to use as pre-trained model 
-TEST_MODEL = None
+# .pkl file of model to test (not the params-file)
+TEST_MODEL = None # e.g. 'BirdCLEF_TUC_CLO_EXAMPLE_model_epoch_50.pkl'
 
-# Maximum amount of randomly selected files from the test set
+# Maximum amount of randomly selected files from the local validation set
 MAX_TEST_FILES = 500
 
 # Limit the amount of test files per class
